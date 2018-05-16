79817c5fd98b9578341ef2cdfe24ebeeff4f7e14
Moogrob <38891751+Moofrob@users.noreply.github.com>
2018-05-16 13:22:36 +0700

    pei

--- NewRelease.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(file_exists("NewRelease.html") && filesize("NewRelease.html") > 0){
-    $myfile = fopen("NewRelease.html","r");
-    echo fread($myfile,filesize("NewRelease.html"));
-    fclose($myfile);
-}
-?>
